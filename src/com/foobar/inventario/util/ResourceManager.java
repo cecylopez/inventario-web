26a499c99c06e84a320af64330702876c466d63c
cecylopez <ceciliac86548@gmail.com>
2017-09-30 15:43:14 -0600

    20: Deployment de la aplicación en Heroku
    Adición de log para visualizar la URL cargada para el recurso
    
    Task-Url: https://github.com/cecylopez/inventario-web/issues/20

--- src/com/foobar/inventario/util/ResourceManager.java
+++ src/com/foobar/inventario/util/ResourceManager.java
@@ -3,10 +3,16 @@ package com.foobar.inventario.util;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
 
 public class ResourceManager {
+	protected static Logger logger = Logger.getLogger(ResourceManager.class);
 
 	public static InputStream getResource(String resourceName) throws FileNotFoundException {
-		return new FileInputStream(ClassLoader.getSystemResource(resourceName).getPath());
+		URL resourceURL = ClassLoader.getSystemResource(resourceName);
+		logger.debug("resourceURL: " + resourceURL);
+		return new FileInputStream(resourceURL.getPath());
 	}
 }
