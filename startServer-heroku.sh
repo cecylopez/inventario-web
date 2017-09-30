f38abfaae234e449d1443e904a733ff80f88a191
cecylopez <ceciliac86548@gmail.com>
2017-09-30 14:37:43 -0600

    20: Deployment de la aplicación en Heroku
    Eliminacion de dependencias para cargo y correcion de la ubicacion de
    <deployables>
    
    Task-Url: https://github.com/cecylopez/inventario-web/issues/20

--- startServer-heroku.sh
+++ startServer-heroku.sh
@@ -4,7 +4,6 @@ export CATALINA_BASE
 
 #copy over the Heroku config files
 cp ./server-heroku.xml ./target/cargo/configurations/tomcat6x/conf/server.xml
-cp ./persistence-heroku.xml ./target/cargo/configurations/tomcat6x/webapps/ROOT/WEB-INF/classes/META-INF/persistence.xml
 
 #make the Tomcat scripts executable
 chmod a+x ./target/cargo/installs/apache-tomcat-6.0.18/apache-tomcat-6.0.18/bin/*.sh
