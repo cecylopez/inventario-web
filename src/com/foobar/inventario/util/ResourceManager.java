70b1b2b71702caa446f07e09f11f7cb56dc4ff1c
cecylopez <ceciliac86548@gmail.com>
2017-10-13 23:03:41 -0600

    20: Deployment de la aplicación en Heroku
    Se le agregaron sysout para poder verificar si los recursos se cargan	
    
    Task-Url: https://github.com/cecylopez/inventario-web/issues/20

--- src/com/foobar/inventario/util/ResourceManager.java
+++ src/com/foobar/inventario/util/ResourceManager.java
@@ -8,11 +8,23 @@ import java.net.URL;
 import org.apache.log4j.Logger;
 
 public class ResourceManager {
+	 public static final String RECURSO = "menu.json";
 	protected static Logger logger = Logger.getLogger(ResourceManager.class);
 
 	public static InputStream getResource(String resourceName) throws FileNotFoundException {
 		URL resourceURL = ClassLoader.getSystemResource(resourceName);
 		logger.debug("resourceURL: " + resourceURL);
+		System.out.println("testStream1: "+ ResourceManager.class.getResourceAsStream(RECURSO) );
+
+		System.out.println("testStream2:"+ ResourceManager.class.getResource(RECURSO));
+		System.out.println("testStream3: "+ ClassLoader.getSystemClassLoader().getResourceAsStream(RECURSO) );
+		System.out.println("testStream4: "+ ResourceManager.class.getResourceAsStream("/" + RECURSO) );
+		System.out.println("testStream5: "+ ResourceManager.class.getResourceAsStream("/com/foobar/inventario/util/" + RECURSO) );
+		System.out.println("testStream6: "+ ResourceManager.class.getResourceAsStream("/inventario-web-0.0.1-SNAPSHOT/WEB-INF/classes/com/foobar/inventario/util/" + RECURSO) );
+		System.out.println("testStream7: "+ ResourceManager.class.getResourceAsStream("/target/inventario-web-0.0.1-SNAPSHOT/WEB-INF/classes/com/foobar/inventario/util/" + RECURSO) );
+
 		return new FileInputStream(resourceURL.getPath());
 	}
+	
+	
 }
