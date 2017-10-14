d0753c5a0d42efefe5f98234cbf3c654f19a9653
cecylopez <ceciliac86548@gmail.com>
2017-10-13 23:19:49 -0600

    20: Deployment de la aplicación en Heroku
    Se elimino la carpeta test pues no se podian revisar en Heroku y dado
    que los test 1,2,5 si funcionan iremos verificandolos 
    
    Task-Url: https://github.com/cecylopez/inventario-web/issues/20

--- src/com/foobar/inventario/util/ResourceManager.java
+++ src/com/foobar/inventario/util/ResourceManager.java
@@ -1,9 +1,7 @@
 package com.foobar.inventario.util;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.URL;
 
 import org.apache.log4j.Logger;
 
@@ -12,18 +10,11 @@ public class ResourceManager {
 	protected static Logger logger = Logger.getLogger(ResourceManager.class);
 
 	public static InputStream getResource(String resourceName) throws FileNotFoundException {
-		URL resourceURL = ClassLoader.getSystemResource(resourceName);
-		logger.debug("resourceURL: " + resourceURL);
 		System.out.println("testStream1: "+ ResourceManager.class.getResourceAsStream(RECURSO) );
-
 		System.out.println("testStream2:"+ ResourceManager.class.getResource(RECURSO));
-		System.out.println("testStream3: "+ ClassLoader.getSystemClassLoader().getResourceAsStream(RECURSO) );
-		System.out.println("testStream4: "+ ResourceManager.class.getResourceAsStream("/" + RECURSO) );
 		System.out.println("testStream5: "+ ResourceManager.class.getResourceAsStream("/com/foobar/inventario/util/" + RECURSO) );
-		System.out.println("testStream6: "+ ResourceManager.class.getResourceAsStream("/inventario-web-0.0.1-SNAPSHOT/WEB-INF/classes/com/foobar/inventario/util/" + RECURSO) );
-		System.out.println("testStream7: "+ ResourceManager.class.getResourceAsStream("/target/inventario-web-0.0.1-SNAPSHOT/WEB-INF/classes/com/foobar/inventario/util/" + RECURSO) );
 
-		return new FileInputStream(resourceURL.getPath());
+		return ResourceManager.class.getResourceAsStream(resourceName);
 	}
 	
 	
